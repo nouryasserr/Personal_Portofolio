402386def22cce63cfaf893c67395d3a72ffebba
Nour Yasser <142158850+nouryasserr@users.noreply.github.com>
2024-09-05 23:29:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const features = document.querySelectorAll(".feature");
+
+features.forEach((feature) => {
+  const featureIcon = feature.querySelector(".feature-icon");
+  const featureP = feature.querySelector(".feature-p");
+
+  const svgIcons = featureIcon ? featureIcon.querySelectorAll("svg") : [];
+
+  feature.addEventListener("mouseover", () => {
+    feature.style.backgroundColor = "#2e3207";
+    feature.style.color = "#ebeef2";
+    feature.style.borderRadius = "10px";
+
+    if (featureIcon) {
+      featureIcon.style.backgroundColor = "#ebeef2";
+    }
+
+    if (featureP) {
+      featureP.style.color = "#ebeef2";
+    }
+
+    svgIcons.forEach((svg) => {
+      svg.style.fill = "#2e3207";
+    });
+  });
+
+  feature.addEventListener("mouseout", () => {
+    feature.style.backgroundColor = "transparent";
+    feature.style.color = "#0d0d0d";
+    feature.style.borderRadius = "0px";
+
+    if (featureIcon) {
+      featureIcon.style.backgroundColor = "#2e3207";
+    }
+
+    if (featureP) {
+      featureP.style.color = "#0d0d0d";
+    }
+
+    svgIcons.forEach((svg) => {
+      svg.style.fill = "#ebeef2";
+    });
+  });
+});
